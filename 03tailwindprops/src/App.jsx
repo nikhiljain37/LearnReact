e79148b793e2422c28e484d1e2da2797fb096f3b
Nikhil <jainnikhil7746@gmail.com>
2024-03-02 13:59:44 +0530

    basic project

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+  let myObj = {
+    username: "nikhil",
+    age: 21
+  }
+  let newArr = [1, 2, 3]
+
+  // we can pass array and oobject from one to another component using ---{}---
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
+      <Card username="chaiaurcode" btnText="click me" />
+      <Card username="kkkk" />
+    </>
+  )
+}
+
+export default App
